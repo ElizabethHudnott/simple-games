ff581bb860eaf3ab4dadfd0c3a53b37fde7eaf56
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2019-02-18 16:46:03 +0000

    Add messages.

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,44 +1,80 @@
+const maxWrongGuesses = 10;
+
+var player1ScoreBox = $('#player1-score');
+var player2ScoreBox = $('#player2-score');
+var messageBox = $('#message-box');
 var wordLetters = $('#word');
 var wordEntryBox = $('#word-entry-box');
 var wordEntryButton = $('#word-entry-btn');
 
-var wordToGuess;
-var wordLength;
+var wordToGuess, wordLength, numLettersUncovered, numWrongGuesses;
+var player1Score = 0, player2Score = 0;
+var playerToPlay = 1;
+
+function alert(message, style) {
+	messageBox.removeClass('alert-success alert-danger alert-warning');
+	messageBox.addClass('alert-' + style);
+	messageBox.html(message);
+}
+
+function requestWord() {
+	wordToGuess = undefined;
+	alert(`<strong>Player ${playerToPlay}:</strong> Enter a word.`, 'success');
+}
+
+requestWord();
+
+function countSpaces() {
+	numLettersUncovered = 0;
+	var spacePosition = wordToGuess.indexOf(' ');
+	while (spacePosition !== -1) {
+		numLettersUncovered = numLettersUncovered + 1;
+		spacePosition = wordToGuess.indexOf(' ', spacePosition + 1);
+	}
+}
 
 function enterWord(event) {
 	var wordEntered = wordEntryBox.val();
-	var i, newLetter;
+	var letterPosition, newLetterCell;
 
 	if (wordEntered !== '') {
 		if (wordToGuess === undefined) {
 			wordToGuess = wordEntered.toUpperCase();
 			wordLength = wordToGuess.length;
+			countSpaces();
+			numWrongGuesses = 0;
 			wordEntryBox.val('');
 			wordEntryButton.html('Solve It!');
 
 			wordLetters.html('');
-			i = 0;
-			while (i < wordToGuess.length) {
-				if (wordToGuess[i] === ' ') {
-					newLetter = $(`
+			letterPosition = 0;
+			while (letterPosition < wordLength) {
+				if (wordToGuess[letterPosition] === ' ') {
+					newLetterCell = $(`
 						<td class="letter-cell">
 							<div class="h1 no-letter">&nbsp;</div>
 						</td>
 					`);
 				} else {
-					newLetter = $(`
+					newLetterCell = $(`
 						<td class="letter-cell">
 							<div class="h1 shadow letter-tile">
 								<div class="letter-text">
-									${wordToGuess[i]}
+									${wordToGuess[letterPosition]}
 								</div>
 							</div>
 						</td>
 					`);
 				}
-				wordLetters.append(newLetter);
-				i = i + 1;
+				wordLetters.append(newLetterCell);
+				letterPosition = letterPosition + 1;
 			}
+			if (playerToPlay === 1) {
+				playerToPlay = 2;
+			} else {
+				playerToPlay = 1;
+			}
+			alert(`<strong>Player ${playerToPlay}:</strong> Guess a letter or solve the puzzle.`, 'success');
 		} else {
 
 		}
@@ -53,33 +89,58 @@ wordEntryBox.on('keydown', function (event) {
 	}
 });
 
-function guessLetter(event) {
-	if (wordToGuess === undefined) {
-		return;
-	}
-
-	var guessedLetter = $(event.target).html();
-	var containsLetter = false;
-	var guessedAllLetters = true;
-	var wordLetterDiv;
-	var i = 0;
-	while (i < wordLength) {
-		wordLetterDiv = wordLetters.children().eq(i).find('.letter-tile');
-		if (guessedLetter === wordToGuess[i]) {
-			containsLetter = true;
-			wordLetterDiv.addClass('revealed');
-		} else if (wordLetterDiv.html() === '&nbsp;') {
-			guessedAllLetters = false;
-		}
-		i = i + 1;
-	}
-
-	if (!containsLetter) {
-
-	}
-	if (guessedAllLetters) {
-
+function win() {
+	alert(`<strong>Player ${playerToPlay}</strong> narrowly escapes the gallows on this occassion.`, 'success');
+	if (playerToPlay === 1) {
+		player1Score = player1Score + 1;
+		player1ScoreBox.html(player1Score);
+	} else {
+		player2Score = player2Score + 1;
+		player2ScoreBox.html(player2Score);
 	}
 }
 
-$('.letter-btn').on('click', guessLetter);
+function enterLetter(event) {
+	var guessedLetter = $(event.target).html();
+
+	if (wordToGuess === undefined) {
+		var partialWord = wordEntryBox.val() + guessedLetter.toLowerCase();
+		wordEntryBox.val(partialWord);
+		return;
+	}
+
+	var wordLetterDiv;
+	var containsLetter = false;
+	var letterPosition = 0;
+
+	while (letterPosition < wordLength) {
+		if (guessedLetter === wordToGuess[letterPosition]) {
+			wordLetterDiv = wordLetters.children().eq(letterPosition).find('.letter-tile');
+			containsLetter = true;
+			if (wordLetterDiv.hasClass('revealed')) {
+				break;
+			}
+			wordLetterDiv.addClass('revealed');
+			numLettersUncovered = numLettersUncovered + 1;
+		}
+		letterPosition = letterPosition + 1;
+	}
+
+	if (containsLetter) {
+
+	} else {
+		numWrongGuesses = numWrongGuesses + 1;
+
+		if (numWrongGuesses === maxWrongGuesses) {
+
+		} else {
+			alert(`There is no ${guessedLetter}. Be careful. You might get hanged!`, 'warning');
+		}
+	}
+
+	if (numLettersUncovered === wordLength) {
+		win();
+	}
+}
+
+$('.letter-btn').on('click', enterLetter);
